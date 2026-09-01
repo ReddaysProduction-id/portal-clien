e131f6de3d083f4501efcd918b1f7331f18876da
ReddaysProduction-id <rianofficial94@gmail.com>
2026-09-01 22:01:59 +0700

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -9,5 +9,5 @@ const CONFIG_APP = {
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
     googleApiKey: "AIzaSyDGSLi990Ysw9E9zyGHB13khdzxq_OuvvY",
-    whatsappAdmin: "62821000000000" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
+    whatsappAdmin: "081255157334" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
 };
